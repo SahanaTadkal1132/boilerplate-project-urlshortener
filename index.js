0e7edd39b9db7fbd9a0ff31f20831b3270924e5a
SahanaTadkal1132 <sahanatadkal1132@gmail.com>
2025-07-10 16:33:39 +0530

    url shortner microservice

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(cors());
 
@@ -22,3 +24,55 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+const dns = require('dns');
+
+
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// GLOBAL URL DB
+let urlDatabase = {};
+let counter = 1;
+
+// POST SHORTENER
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+
+  try {
+    const urlObj = new URL(originalUrl);
+    dns.lookup(urlObj.hostname, (err) => {
+      if (err) {
+        return res.json({ error: 'invalid url' });
+      }
+
+      const shortUrl = counter++;
+      urlDatabase[shortUrl] = originalUrl;
+
+      return res.json({
+        original_url: originalUrl,
+        short_url: shortUrl
+      });
+    });
+  } catch {
+    return res.json({ error: 'invalid url' });
+  }
+});
+
+// REDIRECT
+app.get('/api/shorturl/:short', (req, res) => {
+  const originalUrl = urlDatabase[req.params.short];
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'No short URL found for given input' });
+  }
+});
+
+
